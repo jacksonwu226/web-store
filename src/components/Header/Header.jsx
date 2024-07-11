e4008f877aefdaab0c1caa15692bab346327010b
Jackson Wu <jacksonwu226@gmail.com>
2024-07-10 21:11:45 -0700

    set up cart side bar and css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React, {useContext} from "react";
 import { CartSidebarContext } from "../../contexts/CartSidebarContext";
-// import {B} from 'react-icons/bs'
 import { BsBag } from "react-icons/bs";
 
 export default function Header() {
   const {isOpen, setIsOpen} = useContext(CartSidebarContext);
   
   return (
-    <div>
+    <header className="bg-pink-200">
       <div>Header</div>
-      <div onClick={() => setIsOpen(!isOpen)}>open/close sidebar</div>
-    </div>
+      <div onClick={() => setIsOpen(!isOpen)} className="cursor-pointer flex relative">
+        <BsBag className="text-2xl"/>
+      </div>
+    </header>
   );
 }

b7352d5695e662e537f06172b1fba1ebc46ebb15
Jackson Wu <jacksonwu226@gmail.com>
2024-07-10 19:00:13 -0700

    added product context

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {createContext, useState, useEffect} from "react";
 
-export default function ProductContext() {
+// create context
+
+export const ProductContext = createContext();
+
+export default function ProductProvider({children}) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+      console.log(data);
+    }
+    fetchProducts();
+  }, [])
   return (
-    <div>ProductContext</div>
+    <ProductContext.Provider value={{products}}>
+      {children}
+    </ProductContext.Provider>
   );
 }

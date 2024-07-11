590c67228cd95d118466366c4e01eb5b84f228c9
Jackson Wu <jacksonwu226@gmail.com>
2024-07-10 21:35:14 -0700

    finished addToCart function in context

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-export default function CartContext() {
+export const CartContext = createContext();
+
+export default function CartProvider({children}) {
+  const [cart, setCart] = useState([]);
+  const addToCart = (product, id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    })
+    if(cartItem) {
+      const newCart = [...cart].map(item => {
+        if(item.id === id){
+          return{...item, amount: cartItem.amount+1};
+        } else{
+          return item;
+        }
+      })
+      setCart(newCart);
+    }else{
+      const newItem = {...product, amount: 1};
+      setCart([...cart, newItem])
+    }
+  }
   return (
-    <div>CartContext</div>
+    <CartContext.Provider value={{addToCart}}>
+      {children}
+    </CartContext.Provider>
   );
 }

197ab98df95a1bd681c8a11b69fc87effd8a0e61
Jackson Wu <jacksonwu226@gmail.com>
2024-07-10 22:26:38 -0700

    changed router to fix a bug where link does not work on CartSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Outlet } from 'react-router-dom';
 
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
@@ -10,22 +11,12 @@ import CartSidebar from './components/CartSideBar/CartSidebar';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/product/:id' ,
-    element: <ProductDetails />
-  }
-]);
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <RouterProvider router={router} />
+      <Outlet />
       <CartSidebar />
       <Footer />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,27 @@ import './index.css'
 import ProductProvider from './contexts/ProductContext'
 import CartSidebarProvider from './contexts/CartSidebarContext'
 import CartProvider from './contexts/CartContext'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+import Product from './components/Product/Product';
+import CartSidebar from './components/CartSideBar/CartSidebar';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {path: '/product/:id', element: <ProductDetails />}
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CartSidebarProvider>
       <CartProvider>
         <ProductProvider>
-          <App />
+          <RouterProvider router={router} />
         </ProductProvider>
       </CartProvider>
     </CartSidebarProvider>
